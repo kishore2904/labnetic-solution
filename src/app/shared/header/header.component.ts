ba90cc6fd8727e14bd9c4b3cf44db7178762d5f0
Kishore <kishore290499@gmail.com>
2024-01-07 16:22:23 +0530

    single page

---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css',
+'./mediaqueries.css']
+})
+
+
+export class HeaderComponent {
+  toggleMenu(): void {
+    const menu: HTMLElement | null = document.querySelector(".menu-links");
+    const icon: HTMLElement | null = document.querySelector(".hamburger-icon");
+
+    if (menu && icon) {
+      menu.classList.toggle("open");
+      icon.classList.toggle("open");
+    }
+  }
+}
