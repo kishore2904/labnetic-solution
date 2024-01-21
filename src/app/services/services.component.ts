2597a124298ade47a539a318d5af00b582e77efc
Kishore <kishore290499@gmail.com>
2024-01-21 16:08:00 +0530

    adding footer

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './services.component.html',
   styleUrl: './services.component.css'
 })
